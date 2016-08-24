d585c76f47784dac4ccea008ad99def4a0a123cc
adam.hunting <adam.hunting@gmail.com>
2016-08-24 08:55:19 -0600

    WIP - First Commit

---
+++ FastFood/FastFood/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFood
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public IEnumerable<Food> Food { get; set; }
+
+        private decimal _price;
+
+        public decimal Price
+        {
+            get
+            {
+                if (Food.Count() > 0)
+                {
+                    return Food.Sum(f => Price);
+                }
+                return 0m;
+            }
+      
+        }
+
+        public Order()
+        {
+            Food = new List<Food>();
+        }
+    }
+}
